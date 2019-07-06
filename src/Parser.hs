3873d5dffc8b80a6e103a16b3e9b18155b73eae6
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-07-06 23:13:35 +0100

    parser combinators

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,9 +1,12 @@
 module Parser where
 
-import Data.Char (isAlpha, isDigit, isSpace)
+import Control.Arrow (first)
+import Data.Char (isAlpha, isDigit)
 
 -- | A token is never empty
 type Token = String
+
+type Parser a = [Token] -> [(a, [Token])]
 
 clex :: String -> [Token]
 clex (c:cs)
@@ -29,3 +32,62 @@ isIdChar c = isAlpha c || isDigit c || (c == '_')
 
 isWhiteSpace :: Char -> Bool
 isWhiteSpace = (`elem` " \t\n")
+
+pLit :: String -> Parser String
+pLit s = pSat (== s)
+
+keywords :: [String]
+keywords = ["let", "letrec", "case", "in", "of", "Pack"]
+
+pVar :: Parser String
+pVar = pSat (\t -> (t `notElem` keywords) && (isAlpha . head) t)
+
+pAlt :: Parser a -> Parser a -> Parser a
+pAlt p1 p2 ts = p1 ts ++ p2 ts
+
+pThen :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
+pThen combine p1 p2 ts =
+  [(combine v1 v2, ts2) | (v1, ts1) <- p1 ts, (v2, ts2) <- p2 ts1]
+
+pThen3 :: (a -> b -> c -> d) -> Parser a -> Parser b -> Parser c -> Parser d
+pThen3 combine p1 p2 p3 ts =
+  [ (combine v1 v2 v3, ts3)
+  | (v1, ts1) <- p1 ts
+  , (v2, ts2) <- p2 ts1
+  , (v3, ts3) <- p3 ts2
+  ]
+
+pThen4 ::
+     (a -> b -> c -> d -> e)
+  -> Parser a
+  -> Parser b
+  -> Parser c
+  -> Parser d
+  -> Parser e
+pThen4 combine p1 p2 p3 p4 ts =
+  [ (combine v1 v2 v3 v4, ts4)
+  | (v1, ts1) <- p1 ts
+  , (v2, ts2) <- p2 ts1
+  , (v3, ts3) <- p3 ts2
+  , (v4, ts4) <- p4 ts3
+  ]
+
+pZeroOrMore :: Parser a -> Parser [a]
+pZeroOrMore p = pOneOrMore p `pAlt` pEmpty []
+
+pOneOrMore :: Parser a -> Parser [a]
+pOneOrMore p = pThen (:) p (pZeroOrMore p)
+
+pOneOrMoreWithSep :: Parser a -> Parser b -> Parser [a]
+pOneOrMoreWithSep pa pb = pThen (:) pa (pZeroOrMore (pThen seq pb pa))
+
+pEmpty :: a -> Parser a
+pEmpty a ts = [(a, ts)]
+
+pApply :: Parser a -> (a -> b) -> Parser b
+pApply p f = (first f <$>) . p
+
+pSat :: (String -> Bool) -> Parser String
+pSat p (t:ts)
+  | p t = [(t, ts)]
+pSat _ _ = []
--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -1,20 +1,70 @@
 module ParserSpec where
 
-import Parser (clex)
+import Parser
+  ( clex
+  , keywords
+  , pApply
+  , pLit
+  , pOneOrMore
+  , pOneOrMoreWithSep
+  , pVar
+  , pZeroOrMore
+  )
 import Test.Hspec
-import Text.Printf (printf)
 
 spec :: SpecWith ()
 spec =
-  describe "lexer" $ do
-    it "drops whitespace" $ clex "  a\t\nb " `shouldBe` ["a", "b"]
-    it "handles empty" $ clex "" `shouldBe` []
-    it "vars" $ clex "a123 test_name" `shouldBe` ["a123", "test_name"]
-    it "numbers" $ clex "123 0932" `shouldBe` ["123", "0932"]
-    it "drops line comment" $ clex "a b -- c d \n e" `shouldBe` ["a", "b", "e"]
-    it "single chars" $ do
-      let ops = ["+", "*", "-", "/", "<", ">", "&", "|"]
-      clex (concat ops) `shouldBe` ops
-    it "double chars" $ do
-      let ops = ["<=", "==", "/=", ">=", "->"]
-      clex (concat ops) `shouldBe` ops
+  describe "spec" $ do
+    describe "lexer" $ do
+      it "drops whitespace" $ clex "  a\t\nb " `shouldBe` ["a", "b"]
+      it "handles empty" $ clex "" `shouldBe` []
+      it "vars" $ clex "a123 test_name" `shouldBe` ["a123", "test_name"]
+      it "numbers" $ clex "123 0932" `shouldBe` ["123", "0932"]
+      it "drops line comment" $
+        clex "a b -- c d \n e" `shouldBe` ["a", "b", "e"]
+      it "single chars" $ do
+        let ops = ["+", "*", "-", "/", "<", ">", "&", "|"]
+        clex (concat ops) `shouldBe` ops
+      it "double chars" $ do
+        let ops = ["<=", "==", "/=", ">=", "->"]
+        clex (concat ops) `shouldBe` ops
+    describe "literal parser" $ do
+      let p = pLit "hello"
+      it "no tokens" $ p [] `shouldBe` []
+      it "no match" $ p ["John", "!"] `shouldBe` []
+      it "match" $
+        p ["hello", "John", "!"] `shouldBe` [("hello", ["John", "!"])]
+    describe "vars parser" $ do
+      let p = pVar
+      it "no tokens" $ p [] `shouldBe` []
+      it "no match" $ p ["1", "213123"] `shouldBe` []
+      it "keywords" $ p keywords `shouldBe` []
+      it "match" $ p ["aa", "ab", "23"] `shouldBe` [("aa", ["ab", "23"])]
+    describe "one or more" $ do
+      let p = pOneOrMore (pLit "a")
+      it "no tokens" $ p [] `shouldBe` []
+      it "no match" $ p ["b", "c"] `shouldBe` []
+      it "match" $
+        p ["a", "a", "b"] `shouldBe` [(["a", "a"], ["b"]), (["a"], ["a", "b"])]
+    describe "zero or more" $ do
+      let p = pZeroOrMore (pLit "a")
+      it "no tokens" $ p [] `shouldBe` [([], [])]
+      it "no match" $ p ["b", "c"] `shouldBe` [([], ["b", "c"])]
+      it "match" $
+        p ["a", "a", "b"] `shouldBe`
+        [(["a", "a"], ["b"]), (["a"], ["a", "b"]), ([], ["a", "a", "b"])]
+    describe "apply" $ do
+      let p = pZeroOrMore (pLit "a") `pApply` length
+      it "no tokens" $ p [] `shouldBe` [(0, [])]
+      it "no match" $ p ["b", "c"] `shouldBe` [(0, ["b", "c"])]
+      it "match" $
+        p ["a", "a", "b"] `shouldBe`
+        [(2, ["b"]), (1, ["a", "b"]), (0, ["a", "a", "b"])]
+    describe "one or more with separator" $ do
+      let p = pOneOrMoreWithSep (pLit "a") (pLit ",")
+      it "no tokens" $ p [] `shouldBe` []
+      it "no match" $ p ["b", "c"] `shouldBe` []
+      it "no separator" $ p ["a", "a", "b"] `shouldBe` [(["a"], ["a", "b"])]
+      it "match" $
+        p ["a", ",", "a", "b"] `shouldBe`
+        [(["a", "a"], ["b"]), (["a"], [",", "a", "b"])]
