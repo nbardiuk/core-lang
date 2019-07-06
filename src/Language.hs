4043f03f1a6ba452cb2416966420ad107f436ee3
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-07-06 17:52:43 +0100

    reformat with hindent

--- src/Language.hs
+++ src/Language.hs
@@ -9,68 +9,76 @@ module Language
   , isAtomicExpr
   , recursive
   , nonRecursive
-  )
-where
+  ) where
 
-data Expr a = EVar Name             -- ^ Variables
-            | ENum Int              -- ^ Numbers
-            | EConstr Int Int       -- ^ Constructor tag arity
-            | EAp (Expr a) (Expr a) -- ^ Applications
-            | ELet                  -- ^ Let(rec) expressions
-                 IsRec              -- ^   True = recursive
-                 [(a, Expr a)]      -- ^   Definitions
-                 (Expr a)           -- ^   Body of let(rec)
-            | ECase                 -- ^ Case expression
-                 (Expr a)           -- ^   Expression to scrutinies
-                 [Alter a]          -- ^   Alternatives
-            | ELam [a] (Expr a)     -- ^ Lambda abstractions
-            deriving (Show)
+data Expr a
+  = EVar Name -- ^ Variables
+  | ENum Int -- ^ Numbers
+  | EConstr Int Int -- ^ Constructor tag arity
+  | EAp (Expr a) (Expr a) -- ^ Applications
+  | ELet
+      IsRec -- ^   True = recursive
+      [(a, Expr a)] -- ^   Definitions
+      (Expr a) -- ^   Body of let(rec)
+  | ECase
+      (Expr a) -- ^   Expression to scrutinies
+      [Alter a] -- ^   Alternatives
+  | ELam [a] (Expr a) -- ^ Lambda abstractions
+  deriving (Show)
 
-type Alter a = (Int       -- ^ tag
-              , [a]       -- ^ bound variables
-              , Expr a    -- ^ expression right to the arrow
-               )
+type Alter a
+   = ( Int -- ^ tag
+     , [a] -- ^ bound variables
+     , Expr a -- ^ expression right to the arrow
+      )
 
 type Name = String
 
 type CoreExpr = Expr Name
+
 type CoreAlt = Alter Name
 
 type IsRec = Bool
+
 recursive, nonRecursive :: IsRec
 recursive = True
+
 nonRecursive = False
 
 bindersOf :: [(a, b)] -> [a]
-bindersOf defns = [ name | (name, rhs) <- defns ]
+bindersOf defns = [name | (name, rhs) <- defns]
 
 rhssOf :: [(a, b)] -> [b]
-rhssOf defns = [ rhs | (name, rhs) <- defns ]
+rhssOf defns = [rhs | (name, rhs) <- defns]
 
 -- | expression with no internal structure
 isAtomicExpr :: Expr a -> Bool
 isAtomicExpr (EVar v) = True
 isAtomicExpr (ENum v) = True
-isAtomicExpr _        = False
+isAtomicExpr _ = False
 
 type Program a = [ScDefn a]
+
 type CoreProgram = Program Name
 
 -- | supercombinator definition
-type ScDefn a = ( Name    -- ^ name
-                , [a]     -- ^ arguments
-                , Expr a  -- ^ definition
-                )
+type ScDefn a
+   = ( Name -- ^ name
+     , [a] -- ^ arguments
+     , Expr a -- ^ definition
+      )
+
 type CoreScDefn = ScDefn Name
 
 -- | standard prelude for Core
 preludeDefs :: CoreProgram
-preludeDefs = [ ("I", ["x"], EVar "x")
-              , ("K", ["x", "y"], EVar "x")
-              , ("K1", ["x", "y"], EVar "y")
-              , ("S", ["f", "g", "x"], EAp (EAp (EVar "f") (EVar "x"))
-                                           (EAp (EVar "g") (EVar "x")))
-              , ("compose", ["f", "g", "x"], EAp (EVar "f")
-                                                 (EAp (EVar "g") (EVar "x")))
-              , ("twice", ["f"], EAp (EAp (EVar "compose") (EVar "f")) (EVar "f"))
-              ]
+preludeDefs =
+  [ ("I", ["x"], EVar "x")
+  , ("K", ["x", "y"], EVar "x")
+  , ("K1", ["x", "y"], EVar "y")
+  , ( "S"
+    , ["f", "g", "x"]
+    , EAp (EAp (EVar "f") (EVar "x")) (EAp (EVar "g") (EVar "x")))
+  , ("compose", ["f", "g", "x"], EAp (EVar "f") (EAp (EVar "g") (EVar "x")))
+  , ("twice", ["f"], EAp (EAp (EVar "compose") (EVar "f")) (EVar "f"))
+  ]
--- src/PrettyPrinter.hs
+++ src/PrettyPrinter.hs
@@ -1,14 +1,24 @@
-module PrettyPrinter (pprint) where
+module PrettyPrinter
+  ( pprint
+  ) where
 
-import           Data.List (intersperse)
-import           Language  (CoreAlt, CoreExpr, CoreProgram, CoreScDefn,
-                            Expr (..), Name, isAtomicExpr)
+import Data.List (intersperse)
+import Language
+  ( CoreAlt
+  , CoreExpr
+  , CoreProgram
+  , CoreScDefn
+  , Expr(..)
+  , Name
+  , isAtomicExpr
+  )
 
-data Iseq = INil
-          | IStr String
-          | IAppend Iseq Iseq
-          | IIndent Iseq
-          | INewline
+data Iseq
+  = INil
+  | IStr String
+  | IAppend Iseq Iseq
+  | IIndent Iseq
+  | INewline
 
 iNil :: Iseq
 iNil = INil
@@ -20,9 +30,9 @@ iStr :: String -> Iseq
 iStr = IStr
 
 iAppend :: Iseq -> Iseq -> Iseq
-iAppend INil s  = s
-iAppend  s INil = s
-iAppend s1 s2   = IAppend s1 s2
+iAppend INil s = s
+iAppend s INil = s
+iAppend s1 s2 = IAppend s1 s2
 
 iNewline :: Iseq
 iNewline = INewline
@@ -33,16 +43,17 @@ iIndent = IIndent
 iDisplay :: Iseq -> String
 iDisplay s = flatten 0 [(s, 0)]
 
-flatten
-  :: Int            -- Current column; 0 for first column
-  -> [(Iseq, Int)]  -- Work list
-  -> String         -- Result
+flatten ::
+     Int -- Current column; 0 for first column
+  -> [(Iseq, Int)] -- Work list
+  -> String -- Result
 flatten _ [] = ""
-flatten col ((INil, _) : seqs) = flatten col seqs
-flatten col ((IStr s, _) : seqs) = s ++ flatten (col + length s) seqs
-flatten col ((IAppend s1 s2, indent) : seqs) = flatten col ((s1, indent) : (s2, indent) : seqs)
-flatten _ ((INewline, indent) : seqs) = '\n' : space indent ++ flatten indent seqs
-flatten col ((IIndent s, _) : seqs) = flatten col ((s, col) : seqs)
+flatten col ((INil, _):seqs) = flatten col seqs
+flatten col ((IStr s, _):seqs) = s ++ flatten (col + length s) seqs
+flatten col ((IAppend s1 s2, indent):seqs) =
+  flatten col ((s1, indent) : (s2, indent) : seqs)
+flatten _ ((INewline, indent):seqs) = '\n' : space indent ++ flatten indent seqs
+flatten col ((IIndent s, _):seqs) = flatten col ((s, col) : seqs)
 
 space :: Int -> String
 space n = replicate n ' '
@@ -57,41 +68,76 @@ pprExpr :: CoreExpr -> Iseq
 pprExpr (EVar v) = iStr v
 pprExpr (ENum n) = iStr (show n)
 pprExpr (EAp e1 e2) = iConcat [pprExpr e1, iStr " ", pprAExpr e2]
-pprExpr (ELet isrec defns expr) = iConcat [ iStr keyword, iNewline
-                                          , iStr "  ", iIndent (pprDefns defns), iNewline
-                                          , iStr "in ", pprExpr expr ]
-                                            where keyword = if isrec then "letrec" else "let"
-pprExpr (ECase e alts) = iConcat [ iStr "case ", pprExpr e, iStr " of", iNewline
-                                 , iStr "  ", iIndent (iInterleave iNewline (map pprAlter alts))]
-pprExpr (ELam vs e) = iConcat [ iStr "(\\"
-                              , iInterleave (iStr " ") (map iStr vs)
-                              , iStr " -> ", pprExpr e
-                              , iStr ")"]
+pprExpr (ELet isrec defns expr) =
+  iConcat
+    [ iStr keyword
+    , iNewline
+    , iStr "  "
+    , iIndent (pprDefns defns)
+    , iNewline
+    , iStr "in "
+    , pprExpr expr
+    ]
+  where
+    keyword =
+      if isrec
+        then "letrec"
+        else "let"
+pprExpr (ECase e alts) =
+  iConcat
+    [ iStr "case "
+    , pprExpr e
+    , iStr " of"
+    , iNewline
+    , iStr "  "
+    , iIndent (iInterleave iNewline (map pprAlter alts))
+    ]
+pprExpr (ELam vs e) =
+  iConcat
+    [ iStr "(\\"
+    , iInterleave (iStr " ") (map iStr vs)
+    , iStr " -> "
+    , pprExpr e
+    , iStr ")"
+    ]
 
 pprAlter :: CoreAlt -> Iseq
-pprAlter (tag, vs, e) = iConcat [ iStr "<", iNum tag, iStr "> "
-                                , iInterleave (iStr " ") (map iStr vs)
-                                , iStr " -> "
-                                , pprExpr e]
+pprAlter (tag, vs, e) =
+  iConcat
+    [ iStr "<"
+    , iNum tag
+    , iStr "> "
+    , iInterleave (iStr " ") (map iStr vs)
+    , iStr " -> "
+    , pprExpr e
+    ]
 
 pprDefns :: [(Name, CoreExpr)] -> Iseq
 pprDefns defns = iInterleave sep (map pprDefn defns)
-  where sep = iConcat [iStr ";", iNewline]
+  where
+    sep = iConcat [iStr ";", iNewline]
 
 pprDefn :: (Name, CoreExpr) -> Iseq
 pprDefn (name, expr) = iConcat [iStr name, iStr " = ", iIndent (pprExpr expr)]
 
 pprAExpr :: CoreExpr -> Iseq
-pprAExpr e | isAtomicExpr e = pprExpr e
-           | otherwise      = iConcat [iStr "(",  pprExpr e, iStr ")"]
+pprAExpr e
+  | isAtomicExpr e = pprExpr e
+  | otherwise = iConcat [iStr "(", pprExpr e, iStr ")"]
 
 pprProgram :: CoreProgram -> Iseq
-pprProgram defns = iInterleave (iConcat [iNewline, iNewline]) (map pprScDefn defns)
+pprProgram defns =
+  iInterleave (iConcat [iNewline, iNewline]) (map pprScDefn defns)
 
 pprScDefn :: CoreScDefn -> Iseq
-pprScDefn (name, args, expr) = iConcat [iStr name, iStr " "
-                                     , iInterleave (iStr " ") (map iStr args)
-                                     , iStr " = ", iIndent (pprExpr expr)]
+pprScDefn (name, args, expr) =
+  iConcat
+    [ iStr name
+    , iStr " "
+    , iInterleave (iStr " ") (map iStr args)
+    , iStr " = "
+    , iIndent (pprExpr expr)
+    ]
 
 pprint :: CoreProgram -> String
 pprint prog = iDisplay (pprProgram prog)
